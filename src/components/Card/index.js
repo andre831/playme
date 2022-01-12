59be99e01b11f92ef983d3e05d9cb9d36e0b7cbb
Andre Santos <andredupdm@gmail.com>
2022-01-11 22:45:58 -0300

    add style and frame for card mobile

---
+++ src/components/Card/index.js
@@ -0,0 +1,43 @@
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+
+import './style.min.css'
+import cardmovie from '../../assets/img/cardmovie.jpg'
+
+const Card = (props) => {
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="cardMovie">
+                    <div className="d-flex flex-column flex-md-row justify-content-center align-items-center cardMovie-content">
+                        <img width="200" src={props.poster} />
+                        <div>
+                            <div className="d-flex justify-content-md-start text-start align-items-center cardMovie-content--title">
+                                <Link to="">
+                                    <h3>
+                                        {props.title}
+                                    </h3>
+                                </Link>
+                            </div>
+                           <div  className="d-flex flex-row justify-content-between flex-column flex-md-row align-items-center">
+                                <div className="d-flex flex-column flex-md-row cardMovie-content--buttons">
+                                    <Link to="" className="btn btn-primary">More</Link>
+                                    <Link to="" className="btn btn-primary">Trailer</Link>
+                                </div>
+                                <div className="text-end cardMovie-content--average">
+                                    <p>{props.average}</p>
+                                </div>
+                           </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    );
+}
+
+//<div className="d-flex flex-row justify-content-center justify-content-md-start cardMovie-content--language">
+//<p>Language: {props.language}</p>
+//</div>
+
+export default Card;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,46 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 
-class Home extends Component{
-    render(){
-        return(
-            <div>
-                <h2>lore ipsu</h2>
+import { api, api_key } from "../../services/api";
+import Card from "../../components/Card";
+import Container from "../../components/Container";
+
+const Home = ()=> {
+    const [movies, setMovies] = useState([]);
+ 
+    const getAllMovies = api.get("/movie/now_playing", {
+        params: { api_key }
+    });
+
+    useEffect(() =>{
+        getAllMovies.then(r =>{
+        setMovies(r.data.results);
+        console.log(movies);
+        });
+    }, [])
+
+    const img = (width, path) => `https://image.tmdb.org/t/p/w${width}/${path}`;
+
+    return (
+        <div>
+            <h2>The better of weeka</h2>
+            <div className="d-flex flex-column flex-md-row justify-content-center">  
+                {
+                    movies.map((movie)=>{
+                        return(
+                            <div key={movie.id}>
+                                <Card 
+                                    poster={img(400, movie.poster_path)} 
+                                    title={movie.title}
+                                    language={movie.original_language}
+                                    average={movie.vote_average}
+                                />
+                            </div>
+                        );
+                    })
+                }
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Home;
